6c6e21fc090d5bf85fdb86c56410902c5794beb3
Khushi Desai <khushipdesai@gmail.com>
2025-07-24 03:48:30 +0000

    func

--- cgcom/scripts/__init__.py
+++ cgcom/scripts/__init__.py
@@ -2,16 +2,16 @@ from .train import (
     train_model,
     apply_lr_prior,
     build_graph_from_spatial_data,
-    generate_subgraphs,
     build_dataloaders,
-    get_cell_communication_scores
+    get_cell_communication_scores,
+    generate_subgraph_features
 )
 
 __all__ = [
     'train_model', 
     'apply_lr_prior', 
     'build_graph_from_spatial_data', 
-    'generate_subgraphs', 
     'build_dataloaders',
-    'get_cell_communication_scores'
+    'get_cell_communication_scores',
+    'generate_subgraph_features'
 ]
